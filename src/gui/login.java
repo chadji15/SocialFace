96bfae6097c824f1f353035cc3a113d615578f14
gperic02 <38883767+gperic02@users.noreply.github.com>
2020-11-15 13:19:14 +0200

    sign up added

--- src/gui/login.java
+++ src/gui/login.java
@@ -17,11 +17,12 @@ import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.SystemColor;
 import javax.swing.UIManager;
+import javax.swing.JPasswordField;
 
 public class login extends JPanel {
 	int n;
 	private JTextField usernametext;
-	private JTextField passwordtext;
+	private JPasswordField passwordField;
 
 	/**
 	 * Create the panel.sd
@@ -30,30 +31,25 @@ public class login extends JPanel {
 		setBackground(new Color(153, 153, 255));
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(login.class.getResource("/images/Webp.net-resizeimage (3).png")));
-		lblNewLabel.setBounds(10, 118, 331, 287);
+		lblNewLabel.setBounds(10, 118, 338, 287);
 		add(lblNewLabel);
 		
-		JLabel lblUsername = new JLabel("Username:");
+		JLabel lblUsername = new JLabel("Email:");
 		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblUsername.setBounds(390, 137, 104, 61);
+		lblUsername.setBounds(440, 137, 104, 61);
 		add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password:");
 		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblPassword.setBounds(390, 204, 104, 61);
+		lblPassword.setBounds(440, 204, 80, 61);
 		add(lblPassword);
 		
 		usernametext = new JTextField();
-		usernametext.setBounds(478, 157, 182, 22);
+		usernametext.setBounds(527, 157, 182, 22);
 		add(usernametext);
 		usernametext.setColumns(10);
-		
-		passwordtext = new JTextField();
-		passwordtext.setColumns(10);
-		passwordtext.setBounds(478, 224, 182, 22);
-		add(passwordtext);
 		
 		JButton LoginButton = new JButton("LOGIN");
 		LoginButton.setForeground(SystemColor.windowText);
@@ -63,32 +59,36 @@ public class login extends JPanel {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		LoginButton.setBounds(390, 278, 270, 61);
+		LoginButton.setBounds(430, 278, 279, 61);
 		add(LoginButton);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(0, 0, 352, 521);
+		panel.setBounds(0, 0, 360, 640);
 		add(panel);
 		
 		JButton ForgotButton = new JButton("Forgot Password?");
 		ForgotButton.setForeground(Color.BLACK);
 		ForgotButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		ForgotButton.setBackground(SystemColor.windowBorder);
-		ForgotButton.setBounds(457, 352, 143, 29);
+		ForgotButton.setBounds(501, 352, 143, 29);
 		add(ForgotButton);
 		
 		JButton CreateButton = new JButton("CREATE ACCOUNT");
 		CreateButton.setForeground(Color.BLACK);
 		CreateButton.setFont(new Font("Tahoma", Font.BOLD, 18));
 		CreateButton.setBackground(SystemColor.windowBorder);
-		CreateButton.setBounds(390, 477, 270, 31);
+		CreateButton.setBounds(430, 481, 270, 31);
 		add(CreateButton);
 		
 		JLabel lblDontHaveAccount = new JLabel("Don't have account?");
 		lblDontHaveAccount.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblDontHaveAccount.setBounds(390, 449, 161, 29);
+		lblDontHaveAccount.setBounds(431, 451, 161, 29);
 		add(lblDontHaveAccount);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(527, 224, 182, 22);
+		add(passwordField);
 
 	}
 }
---
+++ src/gui/signup.java
@@ -0,0 +1,125 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class signup extends JPanel {
+	private JTextField firstnametext;
+	private JTextField lastnametext;
+	private JTextField emailtext;
+	private JPasswordField passwordField;
+	private JPasswordField passwordField2;
+
+	/**
+	 * Create the panel.
+	 */
+	public signup() {
+		setBackground(new Color(204, 204, 255));
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 0, 703, 83);
+		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("SIGN UP");
+		lblNewLabel.setForeground(new Color(0, 102, 153));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 28));
+		lblNewLabel.setBounds(289, 13, 145, 65);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(signup.class.getResource("/images/logosmall.png")));
+		lblNewLabel_1.setBounds(0, 0, 187, 140);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblUsername = new JLabel("First Name:");
+		lblUsername.setBounds(186, 96, 97, 36);
+		add(lblUsername);
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		lblLastName.setBounds(186, 145, 97, 36);
+		add(lblLastName);
+		
+		JLabel lblUsername_1_1 = new JLabel("Gender:");
+		lblUsername_1_1.setBounds(186, 194, 97, 36);
+		add(lblUsername_1_1);
+		
+		firstnametext = new JTextField();
+		firstnametext.setBounds(277, 103, 230, 22);
+		add(firstnametext);
+		firstnametext.setColumns(10);
+		
+		lastnametext = new JTextField();
+		lastnametext.setColumns(10);
+		lastnametext.setBounds(277, 152, 230, 22);
+		add(lastnametext);
+		
+		JRadioButton malebutton = new JRadioButton("Male");
+		malebutton.setBackground(new Color(204, 204, 255));
+		malebutton.setBounds(277, 200, 65, 25);
+		add(malebutton);
+		
+		JRadioButton femalebutton = new JRadioButton("Female");
+		femalebutton.setBackground(new Color(204, 204, 255));
+		femalebutton.setBounds(346, 200, 78, 25);
+		add(femalebutton);
+		
+		JRadioButton otherbutton = new JRadioButton("Other");
+		otherbutton.setBackground(new Color(204, 204, 255));
+		otherbutton.setBounds(428, 200, 127, 25);
+		add(otherbutton);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(186, 243, 97, 36);
+		add(lblEmail);
+		
+		emailtext = new JTextField();
+		emailtext.setColumns(10);
+		emailtext.setBounds(277, 250, 230, 22);
+		add(emailtext);
+		
+		JLabel lblBirthday = new JLabel("Birthday:");
+		lblBirthday.setBounds(186, 292, 97, 36);
+		add(lblBirthday);
+		
+		JDateChooser choosedate = new JDateChooser();
+		choosedate.setBounds(277, 298, 230, 22);
+		add(choosedate);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.LEFT);
+		lblPassword.setBounds(186, 341, 97, 36);
+		add(lblPassword);
+		
+		JLabel lblComfirmPassword = new JLabel("Comfirm password:");
+		lblComfirmPassword.setHorizontalAlignment(SwingConstants.LEFT);
+		lblComfirmPassword.setBounds(186, 390, 113, 36);
+		add(lblComfirmPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(311, 348, 196, 22);
+		add(passwordField);
+		
+		passwordField2 = new JPasswordField();
+		passwordField2.setBounds(311, 397, 196, 22);
+		add(passwordField2);
+		
+		JButton createaccountbutton = new JButton("CREATE MY ACCOUNT!");
+		createaccountbutton.setFont(new Font("Tahoma", Font.BOLD, 17));
+		createaccountbutton.setBounds(217, 484, 262, 63);
+		add(createaccountbutton);
+
+	}
+}
