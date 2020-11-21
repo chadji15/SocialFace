ca3e7deec852e4949105b223a6e20793ec833ce7
gperic02 <38883767+gperic02@users.noreply.github.com>
2020-11-21 14:54:18 +0200

    sda

--- sql/procedureinsert.sql
+++ sql/procedureinsert.sql
@@ -1,1 +1,5 @@
-gf
+CREATE PROCEDURE insertrecord
+@LastName nvarchar(20), @FirstName nvarchar(20) , @Birthday [smalldatetime], @Gender [char](1), @Email [nvarchar](50) , @Password [nvarchar](50)
+AS
+ INSERT INTO [dbo].[PROFILE] (profile_id, first_name, last_name, birthday, gender, email, password)
+ VALUES (@@IDENTITY , @FirstName , @LastName , @Birtgday , @Gender , @Email, @Password)
