ef9e8df35bc70e95880f5723f886be749489c80f
chadji15 <chadji15@ucy.ac.cy>
2020-12-05 16:01:29 +0200

    cleanup

--- scripts/temp.py
+++
@@ -1,5 +0,0 @@
-import string
-import random
-
-for _ in range(100):
-    print(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
--- sql/procedureinsert.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE insertrecord
-@LastName nvarchar(20), @FirstName  nvarchar(20) , @Birthday [smalldatetime], @Gender [char](1), @Email [nvarchar](50) , @Password [nvarchar](50)
-AS
- INSERT INTO [atzoum01].[PROFILE] (profile_id, first_name, last_name, birthday, email, password)
- VALUES (@@IDENTITY , @FirstName , @LastName , @Birthday, @Email, @Password)
